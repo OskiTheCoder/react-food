9f16fe6b152a20053d7b3d6cf9d35065198a098c
shelvy <sks91@berkeley.edu>
2021-10-20 14:22:35 -0700

    add mealitem and UI components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,50 @@
-import React from "react";
-import classes from "./AvailableMeals.module.css";
+import React from 'react';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
   {
-    id: "m1",
-    name: "Rainbow Roll",
-    description: "Avocado, cucumber, and cabbage.",
+    id: 'm1',
+    name: 'Rainbow Roll',
+    description: 'Avocado, cucumber, and cabbage.',
     price: 12.99,
   },
   {
-    id: "m2",
-    name: "Garden Roll",
-    description: "Cucumber, tomato, and carrot",
+    id: 'm2',
+    name: 'Garden Roll',
+    description: 'Cucumber, tomato, and carrot',
     price: 12.99,
   },
   {
-    id: "m3",
-    name: "Bento 1",
-    description: "6 piece maki, miso soup, salad, and tempura.",
+    id: 'm3',
+    name: 'Bento 1',
+    description: '6 piece maki, miso soup, salad, and tempura.',
     price: 14.99,
   },
   {
-    id: "m4",
-    name: "Bento 2",
-    description: "Ramen, gyoza, and tempura",
+    id: 'm4',
+    name: 'Bento 2',
+    description: 'Ramen, gyoza, and tempura',
     price: 14.99,
   },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
